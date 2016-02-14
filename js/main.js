81460a41e339ea329e3862f1f7dc6c32927dfed0
Henrik Andersson <henrik.andersson@ume.se>
2016-02-14 16:55:52 +0100

    Add function to draw grid on image

--- js/main.js
+++ js/main.js
@@ -14,5 +14,45 @@ function drawGameImage()
     var ctx = canvas.getContext("2d");
     var img = document.getElementById("first");
     ctx.drawImage(img, 10,10);
+	
+	var imageData = ctx.getImageData(0,0,510,510);
+	ctx.putImageData( addGrid(imageData) ,0,0);
+
 }
 
+//Functions to get index from imagedata object
+function getRedValueIndex(width, x, y)
+{
+	return ((y - 1) * (width * 4)) + ((x - 1) * 4);
+}
+
+function getGreenValueIndex(width, x, y)
+{
+	return getRedValue(width,x,y) + 1;
+}
+
+function getBlueValueIndex(width, x, y)
+{
+	return getRedValue(width, x, y) + 2;
+}
+
+function getAlphaValueIndex(width, x, y)
+{
+	return getRedValue(width, x, y) + 3;
+}
+
+//Function to add grid to image
+function addGrid(imageData)
+{
+	for(var i = 102; i < imageData.data.length; i += 102)
+	{	
+			imageData.data[getRedValueIndex(510,i,11)] = 0;
+			
+			for(var j = 12; j < 510; j++)
+			{
+				imageData.data[getRedValueIndex(510,j,i)] = 0;
+			}
+	}
+	return imageData;
+	
+}
