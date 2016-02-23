510acc8fee3208efcb66b7706c05ad14f9b135d6
Henrik Andersson <henrik.andersson@ume.se>
2016-02-23 10:11:43 +0100

    Add function to upload local image to use as puzzle Image

--- js/main.js
+++ js/main.js
@@ -5,21 +5,33 @@ var moveCanvas;
 //number of pixels to use as border
 var CANVASMARGIN = 10;
 //number of rows and columns of the puzzlepieces
-var NUM_ROWS_COLS_PIECES = 5;
-var correctSolution = new Array(NUM_ROWS_COLS_PIECES*NUM_ROWS_COLS_PIECES);
-var currentPositions = new Array(NUM_ROWS_COLS_PIECES*NUM_ROWS_COLS_PIECES);
+var NUM_COLS_PIECES = 5;
+var NUM_ROWS_PIECES = 5;
+//Max size of puzzle board
+var MAX_IMAGE_WIDTH = 640;
+var MAX_IMAGE_HEIGHT = 480;
+
+var correctSolution = new Array(NUM_COLS_PIECES*NUM_ROWS_PIECES);
+var currentPositions = new Array(NUM_COLS_PIECES*NUM_ROWS_PIECES);
 var grabbedPiece = null;
 var currentBoard;
-var canvasWidth = 800;
-var canvasHeight = 600;
+
+var puzzleImage = null;
+
+var canvasWidth = 1024;
+var canvasHeight = 768;
+
+
 var imageWidth = 490;
 var imageHeight = 490;
+
 $(document).ready(function()
 {
 	$("#startGame").click(startGame);
 	$("#offCanvas").mousedown(mouseDownOnCanvas).mouseup(mouseUpOnCanvas);
 	backgroundCanvas = document.getElementById("gameCanvas").getContext("2d");
 	moveCanvas = document.getElementById("offCanvas").getContext("2d");
+	$("#uploadFile").change(handleUploadFile);
 });
 
 //Starts the game
@@ -29,13 +41,55 @@ function startGame()
 	currentPositions = correctSolution;
 	drawBackground();
 }
+function handleUploadFile()
+{
+	var file = $("#uploadFile").get(0).files[0];
+	var img = document.createElement("img");
+	img.id = "puzzlePic";
+	img.src = window.URL.createObjectURL(file);
+	img.classList.add("hidden");
+	$("#canvasFrame").append(img);
+	$("#puzzlePic").load(function(){
+		var ratio = calculateAspectRatioFit($(this).width(), $(this).height(), MAX_IMAGE_WIDTH, MAX_IMAGE_HEIGHT);
+		var hiddenCanvas = document.createElement("canvas");
+		hiddenCanvas.width = $(this).width();
+		hiddenCanvas.height = $(this).height();
+		hiddenCanvas.id = "hiddenCanvas";
+		hiddenCanvas.classList.add("hidden");
+		$("body").append(hiddenCanvas);
+		var hiddenCtx = $("#hiddenCanvas").get(0).getContext("2d");
+		hiddenCtx.scale(ratio.width / hiddenCanvas.width, ratio.height / hiddenCanvas.height);
+		hiddenCtx.drawImage(img,0,0);
+		puzzleImage = hiddenCtx.getImageData(0,0,ratio.width,ratio.height);
+		imageWidth = ratio.width;
+		imageHeight = ratio.height;
+	})
+
+}
+
+
+//http://stackoverflow.com/questions/3971841/how-to-resize-images-proportionally-keeping-the-aspect-ratio
+function calculateAspectRatioFit(srcWidth, srcHeight, maxWidth, maxHeight) {
+
+    var ratio = Math.min(maxWidth / srcWidth, maxHeight / srcHeight);
+
+    return { width: srcWidth*ratio, height: srcHeight*ratio };
+}
 
 //Draws image on canvas
 function drawGameImage()
 {
-    var img = document.getElementById("first");
-    backgroundCanvas.drawImage(img, CANVASMARGIN,CANVASMARGIN);
-	createPuzzlePieces(img.width, img.height, backgroundCanvas);
+	if(puzzleImage == null)
+    {
+		var img = document.getElementById("first");
+		backgroundCanvas.drawImage(img, CANVASMARGIN,CANVASMARGIN);
+		createPuzzlePieces(img.width, img.height, backgroundCanvas);
+	}
+	else
+	{
+		backgroundCanvas.putImageData(puzzleImage,CANVASMARGIN,CANVASMARGIN);
+		createPuzzlePieces(imageWidth, imageHeight, backgroundCanvas);
+	}
 	currentBoard = backgroundCanvas.getImageData(0,0,canvasWidth,canvasHeight);
 	backgroundCanvas.clearRect(0,0,canvasWidth,canvasHeight);
 
@@ -87,8 +141,8 @@ function getMouseCoordsOnCanvas(event)
 //Function to create puzzlepieces
 function createPuzzlePieces(imageWidth, imageHeight, context){
 
-	var puzzlePieceWidth = imageWidth/NUM_ROWS_COLS_PIECES;
-	var puzzlePieceHeight = imageHeight/NUM_ROWS_COLS_PIECES;
+	var puzzlePieceWidth = imageWidth/NUM_COLS_PIECES;
+	var puzzlePieceHeight = imageHeight/NUM_ROWS_PIECES;
 	var id = 0;
 	for(var i =0;i<imageHeight;i+=puzzlePieceHeight)
 	{
@@ -177,7 +231,7 @@ function puzzlePiece(id, x, y, width, height,imageData){
 	this.pixels = imageData;
 	this.grabbed = false; //IF grabbed, do not draw at background....
 	this.animate = function(coords) {
-		moveCanvas.clearRect(0,0,800,600);
+		moveCanvas.clearRect(0,0,canvasWidth,canvasHeight);
 		moveCanvas.fillRect(coords[0],coords[1],width,height);
 		moveCanvas.putImageData(this.pixels,coords[0],coords[1]);
 		}
